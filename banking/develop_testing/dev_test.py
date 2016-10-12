84c0879728369995f494cb1ae7b482c27e0e6ab0
Javier Garcia <javier.macro.trader@gmail.com>
2016-10-11 22:06:42 -0500

    000

--- banking/develop_testing/dev_test.py
+++ banking/develop_testing/dev_test.py
@@ -1,35 +1,32 @@
 
-
-import credit.prepayment as prepay_model
 import credit.constructor as constructor
+import credit.prepayment as prepay_model
 import interest_rates.models as i_models
 
 
-
 if __name__ == '__main__':
-    proyeccion = 36
+    proyeccion = 11
     producto = 'Vehiculos'
     plazo = 36
     tasas = i_models.InterestRateModel.fixed(plazo, 0.0145)
     prepago = prepay_model.PrepaymentModel.zero(plazo)
-    
-    modelo_credito = constructor.CreditModel.simple(plazo)
+
+    modelo_credito = constructor.CreditModel.simple(nper=plazo,
+                                                    loss=0.0)
     presupuesto = [70000] * proyeccion
-    
-    
-    cont_conditions = dict(balance = 0.0,
-                           ppmt = [0.0 / plazo] * plazo,
-                           fixed_rate = [0.012] * plazo,
-                           spread_DTF = None,
-                           spread_IBR = None,
-                           repricing = None)
 
+    cont_conditions = dict(balance=0.0,
+                           ppmt=[0.0 / plazo] * plazo,
+                           fixed_rate=[0.012] * plazo,
+                           spread_DTF=None,
+                           spread_IBR=None,
+                           repricing=None)
 
+    cosecha_contractual = constructor.contractual_vintage(
+        cont_conditions, prepago, modelo_credito)
 
-    cosecha_contractual = constructor.contractual_vintage(cont_conditions, prepago, modelo_credito)
-    
     cosechas = constructor.collection_of_vintages(producto, proyeccion, plazo, tasas, prepago,
-                                      modelo_credito, presupuesto, 'consolidate')
+                                                  modelo_credito, presupuesto, 'consolidate')
 
-    cosechas_total = cosechas.add(cosecha_contractual, fill_value = 0)
+    cosechas_total = cosechas.add(cosecha_contractual, fill_value=0)
     constructor.print_vintage(cosechas_total)
