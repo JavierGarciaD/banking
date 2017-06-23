eb7f6f53feb86ee72faef2c32ed6c4231fd102ad
Javier Garcia <javier.macro.trader@gmail.com>
2017-06-22 21:32:29 -0500

    test

--- tests/test_interest_rates/test_conversion.py
+++ tests/test_interest_rates/test_conversion.py
@@ -12,33 +12,31 @@ import datetime as dt
 from interest_rates.conversion import compound_effective_yr
 from interest_rates.conversion import ea_a_nmv
 
+sdate = pd.to_datetime("01-31-2017")
 
 @pytest.fixture()
 def vector_fixed10():
-    sdate = pd.to_datetime("01-31-2017")
     dates = pd.date_range(start = sdate, periods = 10, freq = 'M')
     return pd.Series([0.02] * 10, index = dates)
 
 
 @pytest.fixture()
 def vector_fixed12():
-    sdate = pd.to_datetime("01-31-2017")
     dates = pd.date_range(start = sdate, periods = 12, freq = 'M')
     return pd.Series([0.05] * 12, index = dates)
 
 
 @pytest.fixture()
 def vector_variable12():
-    sdate = pd.to_datetime("01-31-2017")
     dates = pd.date_range(start = sdate, periods = 12, freq = 'M')
     data = np.round([(each_step / 100) + 0.1 for each_step in range(12)], 6)
     return pd.Series(data = data, index = dates)
 
 
-def test_componer_diff_size(a = vector_fixed10, b = vector_variable12):
+def test_componer_diff_size(vector_fixed10, vector_variable12):
     with pytest.raises(ValueError):
-        compound_effective_yr(fixed_spreads = a,
-                              repriced_spread = b,
+        compound_effective_yr(fixed_spreads = vector_fixed10,
+                              repriced_spread = vector_variable12,
                               repricing = 1)
 
 
